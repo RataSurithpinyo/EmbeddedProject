079cd9bb5e240ee7dae4438071aaf4c766f0ac04
Rata Surithpinyo <rataprim30026@gmail.com>
2022-06-01 00:23:23 +0700

    add arduino and stm32 code

---
+++ arduino/FinalFile1.ino
@@ -0,0 +1,117 @@
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+#include <SoftwareSerial.h>
+SoftwareSerial NodeSerial(D2, D3); // RX | TX
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "Prim"
+#define WIFI_PASSWORD "primpunn"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyA-L6Ux5y1wRgTQeGZR0rmkurYtw1GLQO8"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://esp-firebase-demo-b70bb-default-rtdb.asia-southeast1.firebasedatabase.app/" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+void setup(){
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("firebase_ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+  pinMode(D2, INPUT);
+  pinMode(D3, OUTPUT);
+  Serial.begin(9600);
+  NodeSerial.begin(9600);
+  Serial.println();
+  Serial.println("NodeMCU/ESP8266 Run");
+}
+
+void loop(){
+  String pData="";
+  int dist = 0;
+  int light = 0;
+  while (NodeSerial.available() > 0) {
+    if (NodeSerial.read() == 255) {
+      dist = NodeSerial.read()*100 + NodeSerial.read();
+      light = NodeSerial.read()*100 + NodeSerial.read();
+      Serial.println(String(dist) + " " + String(light));
+    }
+    //pData+=(char)(nc);
+    //Serial.println("HEre");
+  }
+  delay(1000);
+
+  
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "test/Dist", dist)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setInt(&fbdo, "test/Light", light)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+}
